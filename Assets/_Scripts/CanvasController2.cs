5c95882f726768cb5f0c63fe6107be946b7471a1
ManuJorgea <69644681+ManuJorgea@users.noreply.github.com>
2023-10-11 19:02:18 -0500

    Corregir error botón level 1

--- Assets/_Scripts/CanvasController2.cs
+++ Assets/_Scripts/CanvasController2.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class CanvasController2 : MonoBehaviour
 {
+    public void PlayGame01()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Level2");
